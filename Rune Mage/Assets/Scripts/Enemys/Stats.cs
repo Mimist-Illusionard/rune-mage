e7b290c26119a8f60a8f2505eacca38736aa389c
RestartYC <neo61483@gmail.com>
2021-09-07 20:31:03 +0300

    Enemy`s main

---
+++ Rune Mage/Assets/Scripts/Enemys/Stats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    /* foreach (var field in typeof(StatsCreature).GetFields())
+        {
+            field.SetValue(b, field.GetValue(creature));
+        } */
+
+    public string Name;
+    public GameObject Model;
+    public float RotateSpeed;
+    public float Cost;
+    public float SpawnTime;
+    [Header("Stats")]
+    public float Health;
+    //[Range(0.0001f,0.1f)]
+    public float Speed;
+    public float Attack;
+    public float AttackSped;
+    public float AttackRange;
+    [Header("ModelSettings")]
+    public float Radius;
+}
