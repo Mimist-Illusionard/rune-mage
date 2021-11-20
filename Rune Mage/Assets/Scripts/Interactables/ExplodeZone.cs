4ef3b097516f8bbd596cd37135943b0b767d9a7d
Illusion <canada190@mai.ru>
2021-11-21 02:13:38 +0300

    Add: - New spells: Explode, Fireball, Firebolt, Firestream
    Change:
    - Scene Hub_2

---
+++ Rune Mage/Assets/Scripts/Interactables/ExplodeZone.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using DG.Tweening;
+
+
+public class ExplodeZone : Interactable, IDamage, IInitialize
+{
+    [SerializeField] private Vector3 _scaleEndPosition;
+    [SerializeField] private float _scaleTime;
+    [SerializeField] private bool _isIgnorePlayer;
+
+    public float Damage { get; set; }
+
+    private void Start()
+    {
+        transform.DOScale(_scaleEndPosition, _scaleTime).OnComplete(() => transform.DOScale(new Vector3(0f,0f,0f), 0.25f).OnComplete(() => Destroy(gameObject)));
+    }
+
+    protected override void OnEnter(Collider other)
+    {
+        if (!other.TryGetComponent<Health>(out var health)) return;
+        if (_isIgnorePlayer && other.CompareTag("Player")) return;
+
+        health.RemoveHealth(Damage);
+    }
+
+    protected override void OnExit(Collider other)
+    {       
+    }
+
+    public void Initialize()
+    {
+    }
+}
